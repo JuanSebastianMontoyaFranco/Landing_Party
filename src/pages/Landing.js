1a4283e1f65cec7b48cd9853aeb5466bd7029d2e
Juan Sebastian Montoya Franco <juansmontoyaf@gmail.com>
2025-01-07 16:00:23 -0500

    Adding Changes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -15,9 +15,11 @@ import guest from '../assets/img/section4/person-icon.png';
 import assistance from '../assets/img/section4/asistance.png';
 import reception from '../assets/img/section4/reception.png';
 import guestTitle from '../assets/img/section4/title-persons.png';
-import assistanceTitle from '../assets/img/section4/title-assitance.png';
+import assistanceTitle from '../assets/img/section4//title-assistance.png';
 import receptionTitle from '../assets/img/section4/title-reception.png';
 import backgroundSection4 from '../assets/img/section4/background.jpg';
+import separatorSection4 from '../assets/img/section4/separator.png';
+import smileTitle from '../assets/img/section4/title_smile.png';
 
 //Section 5
 import persons from '../assets/img/section5/person.png';
@@ -27,12 +29,17 @@ import icon2 from '../assets/img/section5/icon2.png';
 import icon3 from '../assets/img/section5/icon3.png';
 import icon4 from '../assets/img/section5/icon4.png';
 import backgroundSection5 from '../assets/img/section5/background.jpg';
+import formal from '../assets/img/section5/formal.png';
 
 //Section 6
 import timeLine from '../assets/img/section6/time-line.png';
+import decoration1 from '../assets/img/section6/decoration1.png';
+import decoration2 from '../assets/img/section6/decoration2.png';
+import see from '../assets/img/section6/see.png';
 
 // Footer
 import logoFooter from '../assets/img/footer/icon.png';
+import fifty from '../assets/img/footer/fifty.png';
 import './style.css'
 
 const Landing = () => {
@@ -82,17 +89,17 @@ const Landing = () => {
             >
                 <img src={separator2} alt="Separador superior" />
 
-                <p className="text-dark">
+                <p className="text-dark letter bold">
                     Quiero dar gracias a Dios por permitirme cumplir este sueño tan especial: llegar a mis 15
                     años rodeada de amor y bendiciones. Este momento único en mi vida no estaría completo
                     sin compartirlo con mi familia y las personas que más quiero.
                 </p>
-                <p className="text-dark">
+                <p className="text-dark letter bold">
                     Por eso, con mucha emoción, quiero invitarlos a mi fiesta de 15 años para celebrar juntos
                     este día tan especial. Será una noche inolvidable llena de alegría y diversión, y me encantaría
                     contar con su presencia para hacerla aún más mágica.
                 </p>
-                <p className="text-dark">
+                <p className="text-dark letter bold">
                     ¡Los espero!
 
                 </p>
@@ -114,6 +121,23 @@ const Landing = () => {
                 }}
             >
                 <Container>
+                    <Row>
+                        <img
+                            style={{
+                                width: '100%', // Cambiar tamaño de la imagen
+                                maxWidth: '600px', // Tamaño máximo de la imagen
+                                margin: '0 auto 15px', // Centrar la imagen y añadir margen inferior
+                                display: 'block',
+                                borderRadius: '10px',
+                            }}
+                            src={smileTitle}
+                            alt="Banner image"
+                            className="img-fluid banner-image"
+                        />
+                    </Row>
+                    <row>
+                        <img src={separatorSection4} alt="Separador superior" />
+                    </row>
                     <Row className="justify-content-center">
                         <Col xs={12} className="text-center">
                             <div className="icon icon-shape icon-lg border-light">
@@ -133,18 +157,7 @@ const Landing = () => {
                                 src={guestTitle}
                                 alt="Descripción 1"
                             />
-                            <div className="guests-number" style={{
-                                position: 'absolute',
-                                top: '9%',
-                                left: '50%',
-                                transform: 'translate(-50%, -50%)', // Centra el número
-                                fontSize: '2rem',
-                                fontWeight: 'bold',
-                                color: '#fff',
-                                padding: '5px 10px',
-                                borderRadius: '5px',
-                                zIndex: '1', // Asegura que el número se quede por encima de la imagen
-                            }}>
+                            <div className="guests-number letter">
                                 {guestsNumber}
                             </div>
                         </Col>
@@ -164,11 +177,18 @@ const Landing = () => {
                                 className="img-fluid mb-4"
                             />
                             <div className=" flex-grow-1 d-flex flex-column align-items-center">
-                                <p className="text-secondary">Eventos Ipus</p>
-                                <p className="text-secondary">Pance</p>
-                                <p className="text-secondary">Cll 16b#127-90</p>
+                                <p className="text-secondary letter bold">Eventos Ipus</p>
+                                <p className="text-secondary letter bold">Pance</p>
+                                <p className="text-secondary letter bold">Cll 16b#127-90</p>
                                 <div className="mt-auto">
-                                    <button className="btn btn-primary">Ver mapa</button>
+                                    <a
+                                        href="https://www.google.com/maps/place/Hacienda+Ipus/@3.331207,-76.5341245,17z/data=!3m1!4b1!4m6!3m5!1s0x8e30a6a26053027b:0x6be5ab881871c251!8m2!3d3.331207!4d-76.5341245!16s%2Fg%2F1pzrklfxw?hl=es&entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D" // Enlace a Google Maps
+                                        target="_blank" // Abre el enlace en una nueva pestaña
+                                        rel="noopener noreferrer" // Seguridad al abrir el enlace
+                                    >
+                                        <button style={{ fontFamily: "'Ibarra Real Nova', serif" }} className="btn btn-primary">Ver mapa</button>
+                                    </a>
+
                                 </div>
                             </div>
                         </Col>
@@ -186,13 +206,13 @@ const Landing = () => {
                                 className="img-fluid mb-4"
                             />
                             <div className=" flex-grow-1 d-flex flex-column align-items-center">
-                                <p style={{paddingTop: '20px'}} className="text-secondary">
+                                <p style={{ paddingTop: '20px' }} className="text-secondary letter bold">
                                     Es importante para
                                     mi saber si contaré
                                     con tu presencia
                                 </p>
                                 <div className="mt-auto">
-                                    <button className="btn btn-primary">Confirmar aquí</button>
+                                    <button style={{ fontFamily: "'Ibarra Real Nova', serif" }} className="btn btn-primary">Confirmar aquí</button>
                                 </div>
                             </div>
                         </Col>
@@ -219,7 +239,7 @@ const Landing = () => {
                             alt="Icon"
                             className="small-icon"
                         />
-                        <h2 className="mb-0">DRESS CODE</h2>
+                        <h2 className="mb-0 letter">DRESS CODE</h2>
                         <img
                             style={{ width: '30px', height: 'auto', marginLeft: '10px' }}
                             src={icon4}
@@ -243,7 +263,20 @@ const Landing = () => {
                     className="img-fluid banner-image"
                 />
 
-                <h4 className="text-center">Nos reservamos el color morado y lila.</h4>
+                <img
+                    style={{
+                        width: '80%', // Cambiar tamaño de la imagen
+                        maxWidth: '300px', // Tamaño máximo de la imagen
+                        margin: '0 auto 15px', // Centrar la imagen y añadir margen inferior
+                        display: 'block',
+                        borderRadius: '10px',
+                    }}
+                    src={formal}
+                    alt="Banner image"
+                    className="img-fluid banner-image"
+                />
+
+                <h4 className="text-center letter">Nos reservamos el color morado y lila.</h4>
 
                 {/* Cuadro morado con contenido */}
                 <div
@@ -282,7 +315,7 @@ const Landing = () => {
                                 marginRight: '10px',
                             }}
                         />
-                        <h5 className="text-white mb-0" style={{ textAlign: 'center', flex: 1 }}>
+                        <h5 className="text-white mb-0 letter" style={{ textAlign: 'center', flex: 1}}>
                             LLUVIA DE SOBRES
                         </h5>
                         <img
@@ -299,37 +332,49 @@ const Landing = () => {
                 </div>
             </section>
 
-
-
-
             <section className="banner mb-5">
                 <div className="text-center">
-                    <h2>LINEA DE TIEMPO</h2>
-
+                    <img src={decoration1} alt="Separador superior" />
+                    <h2 className="letter">LÍNEA DE TIEMPO</h2>
+                    <img src={decoration2} alt="Separador superior" />
                 </div>
                 <img src={timeLine} alt="Banner image" className="img-fluid banner-image" />
             </section>
+
+            <img
+                style={{
+                    width: '70%', // Cambiar tamaño de la imagen
+                    maxWidth: '200px', // Tamaño máximo de la imagen
+                    margin: '0 auto 15px', // Centrar la imagen y añadir margen inferior
+                    display: 'block',
+                    borderRadius: '10px',
+                }}
+                src={see}
+                alt="Banner image"
+                className="img-fluid banner-image"
+            />
+
             <section className="countdown-section">
                 <div className="timer-wrapper">
 
                     <div className="timer-container">
                         <div>
-                            <div className="time-box" id="days">{timeLeft.days < 10 ? `0${timeLeft.days}` : timeLeft.days}</div>
+                            <div  className="time-box letter" id="days">{timeLeft.days < 10 ? `0${timeLeft.days}` : timeLeft.days}</div>
                         </div>
                         <div>
-                            <div className="time-box" id="hours">{timeLeft.hours < 10 ? `0${timeLeft.hours}` : timeLeft.hours}</div>
+                            <div  className="time-box letter" id="hours">{timeLeft.hours < 10 ? `0${timeLeft.hours}` : timeLeft.hours}</div>
                         </div>
                         <div>
-                            <div className="time-box" id="minutes">{timeLeft.minutes < 10 ? `0${timeLeft.minutes}` : timeLeft.minutes}</div>
+                            <div className="time-box letter" id="minutes">{timeLeft.minutes < 10 ? `0${timeLeft.minutes}` : timeLeft.minutes}</div>
                         </div>
                     </div>
                 </div>
 
                 <div className="">
                     <div className="labels-container">
-                        <span className="time-label">Días</span>
-                        <span className="time-label">Horas</span>
-                        <span className="time-label">Minutos</span>
+                        <span className="time-label letter">Días</span>
+                        <span className="time-label letter">Horas</span>
+                        <span className="time-label letter">Minutos</span>
                     </div>
                 </div>
             </section>
@@ -341,9 +386,12 @@ const Landing = () => {
                     <Row>
                         <Col className="mb-md-2">
                             <div className="d-flex text-center justify-content-center align-items-center" role="contentinfo">
-                                <p className="font-weight-normal font-small mb-0">15 años
+                                <p className="font-weight-normal font-small mb-0 letter">
                                     <img
-                                        style={{ width: '40px', height: 'auto', marginLeft: '10px', marginRight: '10px' }}
+                                        style={{ width: '80px', height: 'auto', marginLeft: '10px' }}
+                                        src={fifty}></img>
+                                    <img
+                                        style={{ width: '40px', height: 'auto', marginRight: '10px' }}
                                         src={logoFooter}></img>
                                     Luisa Oriana Osorio Vanegas.
                                 </p>
